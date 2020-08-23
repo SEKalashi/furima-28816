d1175b381c3496501bb315c3369c81caacfe19ce
SEKalashinikov <kiaro.mmo@gmail.com>
2020-08-23 16:19:01 +0900

    ユーザー認証機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :authenicate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_kana])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:family_kana])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:birthday])
+  end
 end
