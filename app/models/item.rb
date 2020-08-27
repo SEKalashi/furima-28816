ad357d93605f06c1a3aaaa076cc5a736a30f3306
SEKalashinikov <kiaro.mmo@gmail.com>
2020-08-27 15:07:55 +0900

    マイグレーションを設定し、db:migrateを実施

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,9 @@
 class Item < ApplicationRecord
   has_many :images
   belong_to :user
+
+  with_options presence: true do
+    validates :price, format: { with: /\d/, message: "is invalid. Input half-width integer."}
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
+  end
 end
--- db/migrate/20200827030709_create_items.rb
+++ db/migrate/20200827030709_create_items.rb
@@ -1,7 +1,15 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-
+      t.string :name,             null: false
+      t.text :description,        null: false
+      t.integer :status,          null: false
+      t.integer :shipping_day,    null: false
+      t.integer :delivery_change, null: false
+      t.integer :category,        null: false
+      t.integer :sender,          null: false
+      t.integer :price,           null: false
+      t.references :user,         null: false, foreign_key: true
       t.timestamps
     end
   end
