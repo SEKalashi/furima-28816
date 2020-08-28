11422ef48849be637b8335e82b965a8789dd0db9
SEKalashinikov <kiaro.mmo@gmail.com>
2020-08-28 16:51:42 +0900

    ActiveHash::Base設定モデルのカラム内容を作成

--- db/migrate/20200827030709_create_items.rb
+++ db/migrate/20200827030709_create_items.rb
@@ -1,15 +1,15 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      t.string :name,             null: false
-      t.text :description,        null: false
-      t.integer :status,          null: false
-      t.integer :shipping_time,   null: false
-      t.integer :delivery_charge, null: false
-      t.integer :category,        null: false
-      t.integer :sending_area,    null: false
-      t.integer :price,           null: false
-      t.references :user,         null: false, foreign_key: true
+      t.string :name,                null: false
+      t.text :description,           null: false
+      t.integer :status_id,          null: false
+      t.integer :shipping_time_id,   null: false
+      t.integer :delivery_charge_id, null: false
+      t.integer :category_id,        null: false
+      t.integer :sending_area_id,    null: false
+      t.integer :price,              null: false
+      t.references :user,            null: false, foreign_key: true
       t.timestamps
     end
   end
