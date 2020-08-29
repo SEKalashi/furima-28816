9aea56f79077010b273156b7f45f4f6ce48bf799
SEKalashinikov <kiaro.mmo@gmail.com>
2020-08-29 16:39:08 +0900

    Revert "Formオブジェクト実装に伴い、コントローラーを修正"
    This reverts commit f5b4770c9f3d27a1aba60a3bcf89c6eff5165e88.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,15 +2,15 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :destroy]
 
   def index
-    @items = UserItems.all
+    @items = User.all
   end
 
   def new
-    @items = UserItems.new
+    @items = User.new
   end
 
   def create
-    @items = UserItems.new(items_params)
+    @items = User.new(items_params)
     if @items.save
       redirect_to root_path
     else
@@ -24,7 +24,8 @@ class ItemsController < ApplicationController
   private
 
   def items_params
-    params.require(:user_items).permit(:nickname, :first_name, :first_kana, :family_name, :family_kana, :birthday, :email, :password,:name, :description, :status_id, :shipping_time_id, :delivery_charge_id, :category_id, :sending_area_id, :price, :image)
+    params.require(:user).merge(image: image.id)
+    # .permit(:name, :description, :status_id, :shipping_time_id, :delivery_charge_id, :category_id, :sending_area_id, :price)
     # .merge(user_id: current_user.id).merge(image_id: current_image.id)
   end
 end
