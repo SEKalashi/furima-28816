e3fe95f8efc6f3917d886b806dc259358ddfc726
SEKalashinikov <kiaro.mmo@gmail.com>
2020-09-04 10:35:11 +0900

    ルーティングとコントローラーの作成とオーダーのアソシエーション忘れも修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :items
+  has_one :order
 
   with_options presence: true do
     validates :first_name, :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width characters.' }
