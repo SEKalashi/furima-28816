dcbcb9601969b36ce667ddd6b3afacba58c5431d
SEKalashinikov <kiaro.mmo@gmail.com>
2020-09-04 18:49:51 +0900

    モデル・マイグレーションを生成し、migrateを実行

---
+++ db/migrate/20200904093909_create_sales.rb
@@ -0,0 +1,14 @@
+class CreateSales < ActiveRecord::Migration[6.0]
+  def change
+    create_table :sales do |t|
+      t.string :postal_code,  null: false
+      t.string :address,      null: false
+      t.string :house_number, null: false
+      t.string :house_name
+      t.integer :prefecture,  null: false
+      t.references :order,    null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
