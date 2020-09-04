b7e402400b570a425c00ce80833cb9f161207791
SEKalashinikov <kiaro.mmo@gmail.com>
2020-09-04 17:56:18 +0900

    コントローラーの共通アクションをまとめる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :set_item, only: [:show, :edit, :update]
+
 
   def index
     @items = Item.order('created_at DESC')
@@ -19,17 +21,14 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    item = Item.find(params[:id])
-    if item.update(items_params)
-      redirect_to root_path
+    if @item.update(items_params)
+       redirect_to root_path
     else
       render :edit
     end
@@ -48,5 +47,9 @@ class ItemsController < ApplicationController
 
   def items_params
     params.require(:item).permit(:name, :description, :status_id, :shipping_time_id, :delivery_charge_id, :category_id, :sending_area_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
