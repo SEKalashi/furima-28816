374c21b5d56f9e3e4c8249d268e004d03d6859a6
SEKalashinikov <kiaro.mmo@gmail.com>
2020-09-04 17:25:22 +0900

    rubocop実施修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -29,7 +29,7 @@ class ItemsController < ApplicationController
   def update
     item = Item.find(params[:id])
     if item.update(items_params)
-       redirect_to root_path
+      redirect_to root_path
     else
       render :edit
     end
@@ -38,7 +38,7 @@ class ItemsController < ApplicationController
   def destroy
     item = Item.find(params[:id])
     if item.destroy
-       redirect_to root_path
+      redirect_to root_path
     else
       render :show
     end
