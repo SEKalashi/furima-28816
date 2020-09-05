2c690c1bb202a9da11d02870ab240f2e235b5365
SEKalashinikov <kiaro.mmo@gmail.com>
2020-09-05 17:22:42 +0900

    itemデータをビューに渡し、ビューを表示可能にする

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,11 +2,22 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!, only: [:index, :create]
 
   def index
-      # @item = Item.find(params[:id])
+    @item = Item.find(params[:item_id])
   end
 
   def create
+    @sales = Sale.new(sales_params)
+    if @sales.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
-  
-  
+
+  private
+
+  def sales_params
+    params.require(:sale).permit(:postal_code, :address, :house_number, :house_name, :telephone_number, :prefecture_id).merge(item_id: current_item.id)
+  end
+
 end
